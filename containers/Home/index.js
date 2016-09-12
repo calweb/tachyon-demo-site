fd3b8bf49425808c6053a75b5f91ca30aeb455fb
Calvin Webster <calweb@gmail.com>
2016-09-11 22:37:10 -0400

    using banner component

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -1,11 +1,11 @@
 module.exports = React => ({ state }) => {
-  const { Header, Footer, Card, Nav } = require('../../components')(React)
+  const { Header, Footer, Card, Nav, Banner } = require('../../components')(React)
   return (
     <div>
       <Nav state={state} />
       <Header state={state} />
       <section className="bg-white-60 mw5 mw9-ns center pa3 ph5-ns">
-
+        <Banner state={state} />
             <Card state={state} />
             <Card state={state} />
             <Card state={state} />
